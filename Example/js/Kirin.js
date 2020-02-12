040658bb181e5988f7aae2fb07da89e7e0d97471
Денис <zololotarenko.2015@gmail.com>
2020-02-12 19:49:49 +0200

    update

--- Example/js/Kirin.js
+++ Example/js/Kirin.js
@@ -1,9 +1,31 @@
-function Kirin(func){
-    setTimeout(func || console.log.bind(console, 'Kirin'), 2000)    
+/*utils*/
+function isEventSupported(eventName){
+    let evN = eventName.toLowerCase();
+    let isSupported = false;
+
+    if(typeof evN !== 'string') return false;
+
+    if(eventName in window){
+        isSupported = true;
+    } else if(('on' + evN) in window) {
+        isSupported = 'on' + evN;
+    } else {
+        let el = document.body;
+        isSupported = (evN in el);
+
+        if (!isSupported) {
+            el.setAttribute(evN,'() => {}');
+            isSupported = (typeof(el[eventName]) === 'function');
+
+            el.removeAttribute(evN);
+        }
+    }
+
+    return isSupported;
 }
 
-Kirin.createElement = function(name, attr, child){
-    let el = document.createElement(name);
+function createDOMComponent(el) {
+    let el = document.createElement(type);
     if(attr !== null){
         Object
             .keys(attr)
@@ -26,24 +48,104 @@ Kirin.createElement = function(name, attr, child){
         el.appendChild(child);
     }
 
-    Kirin.toRender = el;
-    return el;
+    createDOMComponent.mount = function (el, container) {
+        container.appendChild(el)
+    }
 }
 
-Kirin.createClass = function(){
-    
-}
+function CompositeComponentWrapper(el, container) {
+    const Component = el.type;
+    const componentInstance = new Component(el.props);
+    const element = componentInstance.render();
 
-Kirin.createRef = function(el){
-    this.renderEL = el;
-}
+    // while (typeof element.type === 'function') {
+    //     element = (new element.type(element.props)).render();
+    // }
 
-Kirin.removeAfter = function(el, time){
-    setTimeout(() => el.remove() ,time)
+    const domComponentInstance = createDOMComponent(element);
+    return createDOMComponent.mount(domComponentInstance, container);
 }
+/*lib*/
+const Kirin = {
+    createElement(type, props, ...children){
+        const element = {
+            type,
+            props: props || {}
+        };
 
-Kirin.render = function(){
-   this.renderEL.appendChild(
-       this.toRender
-    )
-}
+        if (children[0]) {
+            element.props.children = children;
+        }
+
+        return element;
+    },
+    createRef(el){
+        return {
+            current: el || null
+        };
+    },
+    createClass(spec){
+        function Constructor(props) {
+            this.props = props;
+        }
+
+        Constructor.prototype.render = spec.render;
+
+        return Constructor;
+    },
+    render(element, container){
+        let el = createDOMComponent(element);
+        return createDOMComponent.mount(el, container)
+    }
+};
+
+
+// const TopLevelWrapper = function(props) {
+//     this.props = props;
+// };
+//
+// TopLevelWrapper.prototype.render = function() {
+//     return this.props;
+// };
+// const wrapperElement = this.createElement(TopLevelWrapper, element);
+
+
+const MyTitle = Kirin.createClass({
+    render() {
+        return Kirin.createElement('h1', null, this.props.message);
+    }
+});
+
+Kirin.render(
+    Kirin.createElement(MyTitle, { message: 'hey there Feact' }),
+    document.getElementById('root')
+);
+
+// React.createClass({
+//     getInitialState() {
+//         return {
+//             checked: false,
+//         };
+//     },
+//
+//     toggleChecked() {
+//         this.setState((prevState) => (
+//             { checked: !prevState.checked }
+//         ));
+//     },
+//
+//     render() {
+//         const className = this.state.checked ?
+//             'toggle checkbox checked' : 'toggle checkbox';
+//         return (
+//             <div className={className}>
+//             <input
+//         type='checkbox'
+//         name='public'
+//         onClick={this.toggleChecked}
+//             >
+//             <label>Subscribe to weekly newsletter</label>
+//         </div>
+//     );
+//     }
+// });
